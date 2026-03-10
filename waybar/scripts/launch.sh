6cac1c00a2e3ed1efb3585e59b9c9f81d2e46df1
Hazem Magdy <1hazemmagdy@gmail.com>
2026-03-10 12:42:09 +0200

    Added the restart bash: This script restarts waybar so you don't have to logout/login everytime you configure something

--- waybar/scripts/launch.sh
+++ waybar/scripts/launch.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-# SETUP INSTRUCTIONS (for new machines):
-# To bind this script to a keybind in Hyprland, open ~/.config/hypr/hyprland.conf
+# SETUP INSTRUCTIONS :
+# bind this script to a keybind in Hyprland, or the TWM you use, open ~/.config/hypr/hyprland.conf
 # and add the following line under your keybinds section:
 #
 #   bind = $mainMod, R, exec, ~/.config/waybar/scripts/launch.sh
 #
 # Make sure this script is executable:
 #   chmod +x ~/.config/waybar/scripts/launch.sh
+# Now, when you press the specified key combination (e.g., Super + R), it will execute this script,
+# which will kill any existing Waybar instances and start a new one, so you can see the 
+# changes you made to the configuration file without needing to restart your entire session.
 
 killall -9 waybar
 waybar &
